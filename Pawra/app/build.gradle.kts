b0d57e930be4b623261f0125b6e3bb5fd73deb5c
Muhammad Haris <idris.munu@gmail.com>
2023-11-01 10:24:42 +0700

    Sign In design

--- Pawra/app/build.gradle.kts
+++ Pawra/app/build.gradle.kts
@@ -69,4 +69,5 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
 }
--- Pawra/app/src/main/java/id/pawra/Pawra.kt
+++ Pawra/app/src/main/java/id/pawra/Pawra.kt
@@ -3,14 +3,37 @@ package id.pawra
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import id.pawra.ui.navigation.Screen
+import id.pawra.ui.screen.auth.SignInScreen
 import id.pawra.ui.screen.auth.SignUpScreen
 import id.pawra.ui.theme.PawraTheme
 
 @Composable
 fun Pawra(
     modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
 ) {
-    SignUpScreen()
+    NavHost(
+        navController = navController,
+        startDestination = Screen.SignUp.route,
+        modifier = modifier
+    ) {
+        composable(Screen.SignUp.route) {
+            SignUpScreen(
+                navController = navController
+            )
+        }
+        composable(Screen.SignIn.route) {
+           SignInScreen(
+               navController = navController
+           )
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
