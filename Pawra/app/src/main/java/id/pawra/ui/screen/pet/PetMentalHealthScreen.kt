a66e843d7bd4f268327e6111763b9362ba814b72
Muhammad Haris <idris.munu@gmail.com>
2023-11-28 11:14:46 +0700

    analyze form

--- Pawra/app/src/main/java/id/pawra/ui/screen/pet/PetMentalHealthScreen.kt
+++ Pawra/app/src/main/java/id/pawra/ui/screen/pet/PetMentalHealthScreen.kt
@@ -6,6 +6,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
