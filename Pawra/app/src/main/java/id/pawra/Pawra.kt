fcfda7dd0773638e6f534062b9d806b84fe0fef7
Nadhila Shafaresta <nadhshafres@gmail.com>
2023-12-17 09:43:34 +0700

    update string

--- Pawra/app/src/main/java/id/pawra/Pawra.kt
+++ Pawra/app/src/main/java/id/pawra/Pawra.kt
@@ -1,5 +1,6 @@
 package id.pawra
 
+import android.preference.PreferenceManager
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
