430b7fd49ea255fdd52bfd45b37de75111c273e0
Nadhila Shafaresta <nadhshafres@gmail.com>
2023-12-19 17:36:42 +0700

    update ui

--- Pawra/app/src/main/java/id/pawra/ui/screen/home/HomeScreen.kt
+++ Pawra/app/src/main/java/id/pawra/ui/screen/home/HomeScreen.kt
@@ -13,11 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
